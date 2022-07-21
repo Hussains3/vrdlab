8f855b87345825deb3faf20a98818c3227d85544
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-07-21 19:12:18 +0600

    menu issue

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,22 +1,25 @@
 <header>
-    <div class="container">
-        <div class="nav_area">
-            <div class="nav_left">
-                <a href="#"><img src="{{asset('images/logo.png')}}" alt=""></span></a>
-            </div>
-            <!-- Toggle-menu -->
-            <div class="toggle_btn" id="toggle" onclick="toggleMenu()">
-                <span class="iconify" data-icon="ant-design:menu-outlined"></span>
-            </div>
-            <div class="nav_right" id="mobileMenu">
-                <a href="{{route('index')}}">Home</a>
-                <a href="{{route('ourResearchers')}}">Our Researchers</a>
-                <a href="#">Research Area</a>
-                <a href="{{route('publications')}}">Publications</a>
-                <a href="#">Contact Us</a>
-                <!-- <a href="./author.html">Author</a>
+    <div class="header_area">
+
+        <div class="container">
+            <div class="nav_area navbar">
+                <div class="nav_left">
+                    <a href="https://vrdlab.com/"><img src="{{asset('images/logo.png')}}" alt=""></span></a>
+                </div>
+                <!-- Toggle-menu -->
+                <div class="toggle_btn" id="toggle" onclick="toggleMenu()">
+                    <span class="iconify" data-icon="ant-design:menu-outlined"></span>
+                </div>
+                <div class="nav_right" id="mobileMenu">
+                    <a href="{{route('index')}}">Home</a>
+                    <a href="{{route('ourResearchers')}}">Our Researchers</a>
+                    <a href="#">Research Area</a>
+                    <a href="{{route('publications')}}">Publications</a>
+                    <a href="#">Contact Us</a>
+                    <!-- <a href="./author.html">Author</a>
                 <a href="#"><span class="iconify" data-icon="bi:search"></span></a>
                 <a href="#"><span class="iconify" data-icon="ant-design:menu-outlined"></span></a> -->
+                </div>
             </div>
         </div>
     </div>
--- resources/views/publication.blade.php
+++ resources/views/publication.blade.php
@@ -1,67 +1,67 @@
 @extends('layouts.app')
 @section('title')
-    Publications
+Publications
 @endsection
 @section('content')
-    <section class="hero_area_blog">
-        <div class="container">
+<section class="hero_area_blog">
+    <div class="container">
 
-            <div class="hero_blog">
-                <div class="category-text">
-                    <h2>Catergory</h2>
-                </div>
-                @forelse ($categories as $category)
-                <a href="{{route('category', $category->id)}}">{{$category->name}}</a>
-                @empty
-                <p>No category founded.</p>
-                @endforelse
+        <div class="hero_blog">
+            <div class="category-text">
+                <h2>Catergory</h2>
             </div>
+            @forelse ($categories as $category)
+            <a href="{{route('category', $category->id)}}">{{$category->name}}</a>
+            @empty
+            <p>No category founded.</p>
+            @endforelse
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- ======================Blog-Hero===================== -->
+<!-- ======================Blog-Hero===================== -->
 
-    <!-- ===================Blog-Area-start================= -->
-    <section>
-        <div class="blog_area">
-            <div class="container">
-                <div class="row">
-                    @forelse ($articles as $article)
-                    <div class="col-md-6 col-sm-12">
-                        <div class="blog-post">
-                            <div class="blog-img">
-                                <img src="{{asset($article->cover)}}" alt="{{$article->title}}">
+<!-- ===================Blog-Area-start================= -->
+<section>
+    <div class="blog_area">
+        <div class="container">
+            <div class="row">
+                @forelse ($articles as $article)
+                <div class="col-md-6 col-sm-12">
+                    <div class="blog-post">
+                        <div class="blog-img">
+                            <img src="{{asset($article->cover)}}" alt="{{$article->title}}">
+                        </div>
+                        <div class="blog_content">
+                            <div class="blog-category-name">
+                                <p>{{$article->category->name}}</p>
                             </div>
-                            <div class="blog_content">
-                                <div class="blog-category-name">
-                                    <p>{{$article->category->name}}</p>
-                                </div>
-                                <div class="blog_name">
-                                    <a href="">{{$article->title}}</a>
-                                </div>
-                                <div class="blog_author">
+                            <div class="blog_name">
+                                <a href="{{route('publication',$article->id)}}">{{$article->title}}</a>
+                            </div>
+                            <div class="blog_author">
 
 
-                                    <p>by, @foreach ($article->authours as $author)
-                                        <a href="{{$author->link}}">{{$author->name}}</a>
-                                        @endforeach
-                                    </p>
+                                <p>by, @foreach ($article->authours as $author)
+                                    <a href="{{$author->link}}">{{$author->name}}</a>
+                                    @endforeach
+                                </p>
 
-                                </div>
-                                <div class="post_date">
-                                    <p> - {{date('M d, Y', strtotime($article->pub_date))}}</p>
-                                </div>
+                            </div>
+                            <div class="post_date">
+                                <p> - {{date('M d, Y', strtotime($article->pub_date))}}</p>
                             </div>
                         </div>
                     </div>
-                    @empty
-                    <p>No article Found.</p>
-                    @endforelse
                 </div>
+                @empty
+                <p>No article Found.</p>
+                @endforelse
             </div>
         </div>
-        </div>
-    </section>
+    </div>
+    </div>
+</section>
 @endsection
 
 @section('script')
