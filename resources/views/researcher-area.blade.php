b1e2d5a614a9a74930ea4aa22cf23f781dc52656
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-08-22 14:11:53 +0600

    our recharcher area

---
+++ resources/views/researcher-area.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.app')
+@section('title')
+Researcher area
+@endsection
+@section('content')
+<section>
+    <div class="research_area">
+        <div class="container">
+            <div class="search_here">
+                <h2>Search</h2>
+            </div>
+            <div class="search_form">
+                <form action="">
+                    <div class="serach_input">
+                        <div class="search_box">
+                            <input type="text" placeholder="Search Now">
+                        </div>
+                        <div class="search_btn">
+                            <button type="submit">
+                                <span class="iconify" data-icon="bi:search"></span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- =======================Search=================== -->
+
+
+<section>
+    <div class="research_context">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="contex_card">
+                        <div class="contex_img">
+                            <img src="./img/pbulishCard3.jpg" alt="">
+                        </div>
+                        <div class="contex_details">
+                            <h2>Collective Collection</h2>
+                            <p id="context">Libraries are embedded in an increasingly complex network of
+                                information
+                                supply and
+                                demand. Our goal is to provide
+                                evidence and insight to libraries as they organize collections and services within
+                                this changing network environment.
+                                Libraries are embedded in an increasingly complex network of information
+                                supply and
+
+                            </p>
+
+                        </div>
+                        <div class="context_read_more">
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contex_card">
+                        <div class="contex_img">
+                            <img src="./img/pbulishCard3.jpg" alt="">
+                        </div>
+                        <div class="contex_details">
+                            <h2>Collective Collection</h2>
+                            <p id="context">Libraries are embedded in an increasingly complex network of
+                                information
+                                supply and
+                                demand. Our goal is to provide
+                                evidence and insight to libraries as they organize collections and services within
+                                this changing network environment.
+                                Libraries are embedded in an increasingly complex network of information
+                                supply and
+
+                            </p>
+
+                        </div>
+                        <div class="context_read_more">
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contex_card">
+                        <div class="contex_img">
+                            <img src="./img/pbulishCard3.jpg" alt="">
+                        </div>
+                        <div class="contex_details">
+                            <h2>Collective Collection</h2>
+                            <p id="context">Libraries are embedded in an increasingly complex network of
+                                information
+                                supply and
+                                demand. Our goal is to provide
+                                evidence and insight to libraries as they organize collections and services within
+                                this changing network environment.
+                                Libraries are embedded in an increasingly complex network of information
+                                supply and
+
+                            </p>
+
+                        </div>
+                        <div class="context_read_more">
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+</section>
+
+
+<!-- ==============================Recharcer======================== -->
+
+
+@endsection
+
+@section('script')
+@endsection
